061b4634c87eeb884cc6ae8ba85b4e1492f473c2
mauraqp <mauraqoonitah@gmail.com>
2021-03-01 16:23:43 +0700

    view layouts

--- app/Controllers/Pages.php
+++ app/Controllers/Pages.php
@@ -10,9 +10,7 @@ class Pages extends BaseController
             'title' => 'Home | Belajar CI 4'
 
         ];
-        echo view('layout/header', $data);
-        echo view('pages/home');
-        echo view('layout/footer');
+        return view('pages/home', $data);
     }
     public function about()
     {
@@ -20,8 +18,26 @@ class Pages extends BaseController
             'title' => 'About | Belajar CI 4'
 
         ];
-        echo view('layout/header', $data);
-        echo view('pages/about');
-        echo view('layout/footer');
+        return view('pages/home', $data);
+    }
+    public function contact()
+    {
+        $data = [
+            'title' => 'Contact | Belajar CI 4',
+            'alamat' => [
+                [
+                    'tipe' => 'rumah',
+                    'alamat' => 'Jl. bintara',
+                    'kota' => 'bekasi'
+
+                ],
+                [
+                    'tipe' => 'Kantor',
+                    'alamat' => 'Jl. rawamawngun',
+                    'kota' => 'jakarta'
+                ]
+            ]
+        ];
+        return view('pages/contact', $data);
     }
 }
--- app/Views/pages/contact.php
+++ app/Views/pages/contact.php
@@ -2,12 +2,19 @@
 
 <?= $this->section('content'); ?>
 
-
 <div class="container">
     <div class="row">
         <div class="col">
-            <h1>About</h1>
+            <h1>Contact Us</h1>
             <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eum cum repudiandae porro voluptates officia provident dolores quaerat ea maxime amet. Tempora ipsa, corrupti cupiditate molestiae maiores voluptatem repellat a neque!</p>
+
+            <?php foreach ($alamat as $addr) : ?>
+                <ul>
+                    <li><?= $addr['tipe']; ?></li>
+                    <li><?= $addr['alamat']; ?></li>
+                    <li><?= $addr['kota']; ?></li>
+                </ul>
+            <?php endforeach; ?>
         </div>
     </div>
 </div>
