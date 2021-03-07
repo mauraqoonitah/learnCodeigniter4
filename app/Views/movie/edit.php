a82a9636948df5c2e1137f69e3babe1fdccdb40f
mauraqp <mauraqoonitah@gmail.com>
2021-03-07 22:39:42 +0700

    old input value edit

--- app/Views/movie/edit.php
+++ app/Views/movie/edit.php
@@ -14,7 +14,7 @@
                 <div class="row mb-3">
                     <label for="judul" class="col-sm-2 col-form-label">Judul</label>
                     <div class="col-sm-10">
-                        <input type="text" class="form-control <?= ($validation->hasError('judul')) ? 'is-invalid' : ''; ?>" id="judul" name="judul" autofocus value="<?= $movie['judul']; ?>">
+                        <input type="text" class="form-control <?= ($validation->hasError('judul')) ? 'is-invalid' : ''; ?>" id="judul" name="judul" autofocus value="<?= (old('judul')) ? old('judul') : $movie['judul']; ?>">
                         <div class="invalid-feedback">
                             <?= $validation->getError('judul'); ?>
                         </div>
@@ -23,48 +23,48 @@
                 <div class="row mb-3">
                     <label for="poster" class="col-sm-2 col-form-label">Poster</label>
                     <div class="col-sm-10">
-                        <input type="text" class="form-control <?= ($validation->hasError('poster')) ? 'is-invalid' : ''; ?>" id="poster" name="poster" value="<?= $movie['poster']; ?>">
-                        <div class="invalid-feedback">
-                            <?= $validation->getError('poster'); ?>
-                        </div>
+                        <input type="text" class="form-control <?= ($validation->hasError('poster')) ? 'is-invalid' : ''; ?>" id="poster" name="poster" value="<?= (old('poster')) ? old('poster') : $movie['poster']; ?>"">
+                        <div class=" invalid-feedback">
+                        <?= $validation->getError('poster'); ?>
                     </div>
                 </div>
-                <div class="row mb-3">
-                    <label for="overview" class="col-sm-2 col-form-label">Overview</label>
-                    <div class="col-sm-10">
-                        <input type="text" class="form-control <?= ($validation->hasError('overview')) ? 'is-invalid' : ''; ?>" id="overview" name="overview" value="<?= $movie['overview']; ?>">
-                        <div class="invalid-feedback">
-                            <?= $validation->getError('overview'); ?>
-                        </div>
-                    </div>
-                </div>
-                <div class="row mb-3">
-                    <label for="kategori" class="col-sm-2 col-form-label">Kategori</label>
-                    <div class="col-sm-10">
-                        <input type="text" class="form-control <?= ($validation->hasError('kategori')) ? 'is-invalid' : ''; ?>" id="kategori" name="kategori" value="<?= $movie['kategori']; ?>">
-                        <div class="invalid-feedback">
-                            <?= $validation->getError('kategori'); ?>
-                        </div>
-                    </div>
-                </div>
-                <div class="row mb-3">
-                    <label for="tahun" class="col-sm-2 col-form-label">Tahun</label>
-                    <div class="col-sm-10">
-                        <input type="text" class="form-control <?= ($validation->hasError('tahun')) ? 'is-invalid' : ''; ?>" id="tahun" name="tahun" value="<?= $movie['tahun']; ?>">
-                        <div class="invalid-feedback">
-                            <?= $validation->getError('tahun'); ?>
-                        </div>
-                    </div>
-                </div>
-
-
-                <button type="submit" class="btn btn-primary">Ubah Data</button>
-            </form>
-
-
-
         </div>
+        <div class="row mb-3">
+            <label for="overview" class="col-sm-2 col-form-label">Overview</label>
+            <div class="col-sm-10">
+                <input type="text" class="form-control <?= ($validation->hasError('overview')) ? 'is-invalid' : ''; ?>" id="overview" name="overview" value="<?= (old('overview')) ? old('overview') : $movie['overview']; ?>">
+                <div class="invalid-feedback">
+                    <?= $validation->getError('overview'); ?>
+                </div>
+            </div>
+        </div>
+        <div class="row mb-3">
+            <label for="kategori" class="col-sm-2 col-form-label">Kategori</label>
+            <div class="col-sm-10">
+                <input type="text" class="form-control <?= ($validation->hasError('kategori')) ? 'is-invalid' : ''; ?>" id="kategori" name="kategori" value="<?= (old('kategori')) ? old('kategori') : $movie['kategori']; ?>">
+                <div class="invalid-feedback">
+                    <?= $validation->getError('kategori'); ?>
+                </div>
+            </div>
+        </div>
+        <div class="row mb-3">
+            <label for="tahun" class="col-sm-2 col-form-label">Tahun</label>
+            <div class="col-sm-10">
+                <input type="text" class="form-control <?= ($validation->hasError('tahun')) ? 'is-invalid' : ''; ?>" id="tahun" name="tahun" value="<?= (old('tahun')) ? old('tahun') : $movie['tahun']; ?>">
+                <div class="invalid-feedback">
+                    <?= $validation->getError('tahun'); ?>
+                </div>
+            </div>
+        </div>
+
+
+        <button type="submit" class="btn btn-primary">Ubah Data</button>
+        </form>
+
+
+
     </div>
+</div>
 </div>
 
 
