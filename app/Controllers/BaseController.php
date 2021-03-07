c15f7640e143438f45d5276f10c0e1d5c0a57b6d
mauraqp <mauraqoonitah@gmail.com>
2021-03-07 13:37:41 +0700

    getVar basecontroller

--- app/Controllers/BaseController.php
+++ app/Controllers/BaseController.php
@@ -20,6 +20,16 @@ use Psr\Log\LoggerInterface;
 
 class BaseController extends Controller
 {
+
+	/**
+	 * Instance of the main Request object.
+	 *
+	 * @var HTTP\IncomingRequest
+	 */
+	protected $request;
+
+
+
 	/**
 	 * An array of helpers to be loaded automatically upon
 	 * class instantiation. These helpers will be available
