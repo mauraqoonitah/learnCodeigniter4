5476d49ebbc18f3e95ab232a8433269f3dc377cd
mauraqp <mauraqoonitah@gmail.com>
2021-07-22 02:26:38 +0700

    myth/auth login logout

--- app/Views/layout/navbar.php
+++ app/Views/layout/navbar.php
@@ -21,6 +21,15 @@
                 <li class="nav-item">
                     <a class="nav-link" href="/people">People</a>
                 </li>
+                <?php if (logged_in()) : ?>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/logout">Logout</a>
+                    </li>
+                <?php else :  ?>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/login">Login</a>
+                    </li>
+                <?php endif; ?>
 
             </ul>
         </div>
